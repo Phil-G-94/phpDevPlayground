0eca28681b0261666550247bc744e4862719acbc
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-09-08 16:53:16 +0100

    Update README.md

--- web-apps-with-php/cryptomasters/classes/CryptoConverter.php
+++ web-apps-with-php/cryptomasters/classes/CryptoConverter.php
@@ -19,9 +19,9 @@ class CryptoConverter {
         $url = "https://cex.io/api/ticker/$code/USD";
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); # fuckery
+        curl_setopt($ch, CURLOPT_URL, $url); # specify the URL for the request
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); # return result as string instead of dumping to browser/terminal
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); # don't require SSL cert verification
 
         $response = curl_exec($ch);
 
